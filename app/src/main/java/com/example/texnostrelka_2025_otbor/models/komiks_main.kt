7a0b44597dc26b5f4452ca4816f2d6f9fcceab53
Serjik-Developer <urov51450@gmail.com>
2025-03-11 18:47:52 +0300

    удалил ненужное

--- app/src/main/java/com/example/texnostrelka_2025_otbor/models/komiks_main.kt
+++
@@ -1,8 +0,0 @@
-package com.example.texnostrelka_2025_otbor.models
-
-data class komiks_main(
-    val id:String? = null,
-    val text:String? = null,
-    val description: String? = null,
-    val image:String? = null
-)
