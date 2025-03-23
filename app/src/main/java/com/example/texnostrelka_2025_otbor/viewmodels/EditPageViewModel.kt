9007cf3c2b8269383ca98ad8d0d69784a7b6910d
Serjik-Developer <urov51450@gmail.com>
2025-03-23 21:48:06 +0300

    I continue to rewrite my project to MVVM architecture. Now I have rewritten EditPageActivity. I looked at a case where my viewmodel implements 2 different variables and 2 different flows.

---
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/viewmodels/EditPageViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.texnostrelka_2025_otbor.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.texnostrelka_2025_otbor.models.ImageModel
+import com.example.texnostrelka_2025_otbor.models.PageWithImagesIds
+import com.example.texnostrelka_2025_otbor.repositories.ComicsRepository
+import kotlinx.coroutines.launch
+
+class EditPageViewModel(private val pageId: String, private val repository: ComicsRepository) : ViewModel() {
+    private val _pageWithImages = MutableLiveData<PageWithImagesIds>()
+    val pageWithImages: LiveData<PageWithImagesIds> get() = _pageWithImages
+
+    private val _images = MutableLiveData<List<ImageModel>>()
+    val images: LiveData<List<ImageModel>> get() = _images
+
+    fun fetchPageWithImages() {
+        viewModelScope.launch {
+            val page = repository.getMyPage(pageId).find { it.pageId == pageId }
+            if (page != null) {
+                val imageIds = repository.getAllImagesOnPage(pageId).map { it.id!! }
+                _pageWithImages.value = PageWithImagesIds(page, imageIds)
+            }
+        }
+    }
+
+    fun fetchImages() {
+        viewModelScope.launch {
+            val images = repository.getAllImagesOnPage(pageId)
+            _images.value = images
+        }
+    }
+}
