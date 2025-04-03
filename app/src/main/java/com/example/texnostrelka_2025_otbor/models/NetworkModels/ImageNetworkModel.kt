50fcef57ac59a18a46a6f17270141440c61fad8b
Serjik-Developer <urov51450@gmail.com>
2025-04-03 20:44:12 +0300

    changed the format of input data in the data class

---
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/models/NetworkModels/ImageNetworkModel.kt
@@ -0,0 +1,10 @@
+package com.example.texnostrelka_2025_otbor.models.NetworkModels
+
+import android.graphics.Bitmap
+
+data class ImageNetworkModel(
+    val id: String? = null,
+    val pageId: String? = null,
+    val image: String? = null,
+    val cellIndex: Int = -1
+)
--- app/src/main/java/com/example/texnostrelka_2025_otbor/models/NetworkModels/PageFromNetwork.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/models/NetworkModels/PageFromNetwork.kt
@@ -1,11 +1,9 @@
 package com.example.texnostrelka_2025_otbor.models.NetworkModels
 
-import com.example.texnostrelka_2025_otbor.models.ImageModel
-
 data class PageFromNetwork(
     val pageId:String,
     val number:Int,
     val rows:Int,
     val columns:Int,
-    val images: MutableList<ImageModel>? = null
+    val images: MutableList<ImageNetworkModel>? = null
 )
