78f6cd3e04d8f6c7da5e3d2f59b0ec3d0f6501f0
Serjik-Developer <urov51450@gmail.com>
2025-04-17 22:20:00 +0300

    Added some documentation to ApiExceptions.kt. Started working on EditImageNetworkViewModel.kt

--- app/src/main/java/com/example/texnostrelka_2025_otbor/data/remote/exception/ApiExceptions.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/data/remote/exception/ApiExceptions.kt
@@ -1,10 +1,10 @@
 package com.example.texnostrelka_2025_otbor.data.remote.exception
 
-class NotAuthorizedException(message: String) : Exception(message)
-class BadRequestException(message: String) : Exception(message)
-class ApiException(message: String) : Exception(message)
-class NetworkException(message: String) : Exception(message)
-class NotFoundException(message: String) : Exception(message)
-class ConflictException(message: String) : Exception(message)
-class ForbiddenException(message: String) : Exception(message)
-class TooManyRequests(message: String) : Exception(message)
+class NotAuthorizedException(message: String) : Exception(message) //401
+class BadRequestException(message: String) : Exception(message) //400
+class ApiException(message: String) : Exception(message) //500
+class NetworkException(message: String) : Exception(message) //IO EXCEPTION
+class NotFoundException(message: String) : Exception(message) //404
+class ConflictException(message: String) : Exception(message) //409
+class ForbiddenException(message: String) : Exception(message) //403
+class TooManyRequests(message: String) : Exception(message) //429
--- app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/editimagenetwork/EditImageNetworkViewModel.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/editimagenetwork/EditImageNetworkViewModel.kt
@@ -1,9 +1,53 @@
 package com.example.texnostrelka_2025_otbor.presentation.ui.editimagenetwork
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.texnostrelka_2025_otbor.data.local.preferences.PreferencesManager
+import com.example.texnostrelka_2025_otbor.data.remote.exception.BadRequestException
+import com.example.texnostrelka_2025_otbor.data.remote.exception.ForbiddenException
+import com.example.texnostrelka_2025_otbor.data.remote.exception.NetworkException
+import com.example.texnostrelka_2025_otbor.data.remote.exception.NotAuthorizedException
+import com.example.texnostrelka_2025_otbor.data.remote.exception.NotFoundException
+import com.example.texnostrelka_2025_otbor.data.remote.model.image.request.ImageRequestModel
 import com.example.texnostrelka_2025_otbor.data.remote.repository.NetworkRepository
+import kotlinx.coroutines.launch
 
 class EditImageNetworkViewModel(private val repository: NetworkRepository, private val preferencesManager: PreferencesManager) : ViewModel() {
 
+    private val _error = MutableLiveData<String?>()
+    val error : LiveData<String?> get() = _error
+    private val _success = MutableLiveData<String?>()
+    val success : LiveData<String?> get() = _success
+
+    fun addImage(pageId: String, request: ImageRequestModel) {
+        viewModelScope.launch {
+            try {
+                val token = preferencesManager.getAuthToken()
+                if (token.isNullOrEmpty()) {
+                    _error.postValue("Не авторизован.")
+                    return@launch
+                }
+                repository.postImage(pageId, "Bearer $token", request)
+                _success.postValue("Изображение успешно загружено!")
+            } catch (e: BadRequestException) {
+                _error.value = e.message
+            } catch (e: NotAuthorizedException) {
+                preferencesManager.clearAuthToken()
+                preferencesManager.clearName()
+                _error.value = e.message
+            } catch (e: ForbiddenException) {
+                _error.value = e.message
+            } catch (e: NotFoundException) {
+                _error.value = e.message
+            } catch (e: NetworkException) {
+                _error.value = "Проблемы с интернетом"
+            } catch (e: Exception) {
+                _error.value = "Незивестная ошибка"
+                Log.w("EditImageNetworkViewModel", "Unknown error", e)
+            }
+        }
+    }
 }
