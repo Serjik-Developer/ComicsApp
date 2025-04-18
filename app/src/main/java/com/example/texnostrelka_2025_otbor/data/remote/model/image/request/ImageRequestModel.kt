00ce51e02127220546b29729a6ac114118bc5e98
Serjik-Developer <urov51450@gmail.com>
2025-04-18 23:17:18 +0300

    small change

--- app/src/main/java/com/example/texnostrelka_2025_otbor/data/remote/model/image/request/ImageRequestModel.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/data/remote/model/image/request/ImageRequestModel.kt
@@ -1,6 +1,6 @@
 package com.example.texnostrelka_2025_otbor.data.remote.model.image.request
 
 data class ImageRequestModel(
-    val cellIndex: Int? = null,
+    val cellIndex: Int,
     val image: String
 )
