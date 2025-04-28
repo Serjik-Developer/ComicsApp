e2e7d7b278c523ea707b3d5808407297891d36a2
Serjik-Developer <urov51450@gmail.com>
2025-04-28 18:58:58 +0300

    I did a little UserInfoActivity and UserInfoViewModel

--- app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/userinfo/UserInfoActivity.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/userinfo/UserInfoActivity.kt
@@ -1,16 +1,55 @@
 package com.example.texnostrelka_2025_otbor.presentation.ui.userinfo
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.texnostrelka_2025_otbor.databinding.ActivityInfoUserBinding
+import com.example.texnostrelka_2025_otbor.presentation.adapter.ComicsNetworkAdapter
+import com.example.texnostrelka_2025_otbor.presentation.listener.OnItemComicsListener
+import com.example.texnostrelka_2025_otbor.presentation.utils.DialogHelper.showErrorDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class UserInfoActivity : AppCompatActivity() {
+class UserInfoActivity : AppCompatActivity(), OnItemComicsListener {
+    private lateinit var adapter: ComicsNetworkAdapter
     private lateinit var binding: ActivityInfoUserBinding
+    private val viewModel: UserInfoViewModel by viewModels()
+    private var userId: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInfoUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        userId = intent.getStringExtra("USER-ID") ?: throw IllegalArgumentException("USER-ID Is required")
+        adapter = ComicsNetworkAdapter(mutableListOf(), this)
+        viewModel.userData.observe(this) { userData ->
+            adapter.updateData(userData.comics)
+        }
+        viewModel.error.observe(this) { error ->
+            error?.let { showErrorDialog(it) }
+        }
+        viewModel.isSubscribed.observe(this) { isSubscribed ->
+
+        }
+        viewModel.fecthUserdata(userId)
+    }
+
+    override fun onItemClick(id: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDeleteClick(id: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEditClick(id: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSendClick(id: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDownloadClick(id: String) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/userinfo/UserInfoViewModel.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/userinfo/UserInfoViewModel.kt
@@ -1,11 +1,81 @@
 package com.example.texnostrelka_2025_otbor.presentation.ui.userinfo
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.texnostrelka_2025_otbor.data.local.preferences.PreferencesManager
+import com.example.texnostrelka_2025_otbor.data.remote.exception.BadRequestException
+import com.example.texnostrelka_2025_otbor.data.remote.exception.NetworkException
+import com.example.texnostrelka_2025_otbor.data.remote.exception.NotAuthorizedException
+import com.example.texnostrelka_2025_otbor.data.remote.exception.NotFoundException
+import com.example.texnostrelka_2025_otbor.data.remote.model.user.InfoUserResponseModel
 import com.example.texnostrelka_2025_otbor.data.remote.repository.NetworkRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class UserInfoViewModel @Inject constructor(repository: NetworkRepository, preferencesManager: PreferencesManager) : ViewModel() {
+class UserInfoViewModel @Inject constructor(private val repository: NetworkRepository,private val preferencesManager: PreferencesManager) : ViewModel() {
+    private val _userData = MutableLiveData<InfoUserResponseModel>()
+    val userData: LiveData<InfoUserResponseModel> get() = _userData
+    private val _error = MutableLiveData<String?>()
+    val error : LiveData<String?> get() = _error
+    private val _isSubscribed = MutableLiveData<Boolean>()
+    val isSubscribed : LiveData<Boolean> get() = _isSubscribed
+
+    fun fecthUserdata(userId: String) {
+        _error.value = null
+        viewModelScope.launch {
+            try {
+                val token = preferencesManager.getAuthToken()
+                if(token.isNullOrEmpty()){
+                    _error.value = "Не авторизован."
+                    return@launch
+                }
+                _userData.value = repository.getInfoAboutUser(userId, token)
+            } catch (e: NotAuthorizedException) {
+                _error.value = "Не авторизован."
+                preferencesManager.clearName()
+                preferencesManager.clearAuthToken()
+            } catch (e: BadRequestException) {
+                _error.value = "Некорректный запрос"
+            } catch (e: NetworkException) {
+                _error.value = "Проблемы с интернетом"
+            } catch (e: NotFoundException) {
+                _error.value = "Пользователь не найден"
+            } catch (e: Exception) {
+                _error.value = "Неизвестная ошибка"
+                Log.e("InfoComicViewModel", "Unknown error", e)
+            }
+        }
+    }
+
+    fun postSubscribe(userId: String) {
+        _error.value = null
+        viewModelScope.launch {
+            try {
+                val token = preferencesManager.getAuthToken()
+                if(token.isNullOrEmpty()) {
+                    _error.value = "Не авторизован."
+                    return@launch
+                }
+                _isSubscribed.value = repository.postSubscribe(userId, token).subscribed
+            } catch (e: NotAuthorizedException) {
+                _error.value = "Не авторизован."
+                preferencesManager.clearName()
+                preferencesManager.clearAuthToken()
+            } catch (e: BadRequestException) {
+                _error.value = "Нельзя подписаться на самого себя"
+            } catch (e: NetworkException) {
+                _error.value = "Проблемы с интернетом"
+            } catch (e: NotFoundException) {
+                _error.value = "Пользователь не найден"
+            } catch (e: Exception) {
+                _error.value = "Неизвестная ошибка"
+                Log.e("InfoComicViewModel", "Unknown error", e)
+            }
+        }
+    }
 }
