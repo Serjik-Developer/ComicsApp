1e59374ebb902a6a0b26a4166ae33dde3d1c99e1
Serjik-Developer <urov51450@gmail.com>
2025-04-28 20:46:42 +0300

    changed response model

--- app/src/main/java/com/example/texnostrelka_2025_otbor/data/remote/model/subscribe/SubscribeUsersResponseModel.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/data/remote/model/subscribe/SubscribeUsersResponseModel.kt
@@ -2,5 +2,6 @@ package com.example.texnostrelka_2025_otbor.data.remote.model.subscribe
 
 data class SubscribeUsersResponseModel(
     val id: String,
-    val name: String
+    val name: String,
+    val avatar: String
 )
