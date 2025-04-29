9b8e3ef29ddb16d327d9fafc90b2dc6cc2473098
Serjik-Developer <urov51450@gmail.com>
2025-04-29 16:48:41 +0300

    ViewModel supplemented

--- app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/userinfo/UserInfoViewModel.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/userinfo/UserInfoViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.texnostrelka_2025_otbor.presentation.ui.userinfo
 
 import android.util.Log
+import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.texnostrelka_2025_otbor.data.local.preferences.PreferencesManager
@@ -10,6 +12,7 @@ import com.example.texnostrelka_2025_otbor.data.remote.exception.BadRequestExcep
 import com.example.texnostrelka_2025_otbor.data.remote.exception.NetworkException
 import com.example.texnostrelka_2025_otbor.data.remote.exception.NotAuthorizedException
 import com.example.texnostrelka_2025_otbor.data.remote.exception.NotFoundException
+import com.example.texnostrelka_2025_otbor.data.remote.model.subscribe.SubscribeUsersResponseModel
 import com.example.texnostrelka_2025_otbor.data.remote.model.user.InfoUserResponseModel
 import com.example.texnostrelka_2025_otbor.data.remote.repository.NetworkRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -24,6 +27,8 @@ class UserInfoViewModel @Inject constructor(private val repository: NetworkRepos
     val error : LiveData<String?> get() = _error
     private val _isSubscribed = MutableLiveData<Boolean>()
     val isSubscribed : LiveData<Boolean> get() = _isSubscribed
+    private val _subscribeUsers = MutableLiveData<MutableList<SubscribeUsersResponseModel>>()
+    val subscribeUsers : LiveData<MutableList<SubscribeUsersResponseModel>> get() = _subscribeUsers
 
     fun fecthUserdata(userId: String) {
         _error.value = null
@@ -68,6 +73,62 @@ class UserInfoViewModel @Inject constructor(private val repository: NetworkRepos
                 preferencesManager.clearAuthToken()
             } catch (e: BadRequestException) {
                 _error.value = "Нельзя подписаться на самого себя"
+            } catch (e: NetworkException) {
+                _error.value = "Проблемы с интернетом"
+            } catch (e: NotFoundException) {
+                _error.value = "Пользователь не найден"
+            } catch (e: Exception) {
+                _error.value = "Неизвестная ошибка"
+                Log.e("InfoComicViewModel", "Unknown error", e)
+            }
+        }
+    }
+
+    fun fetchSubscribersUsers(userId: String) {
+        _error.value = null
+        viewModelScope.launch {
+            try {
+                val token = preferencesManager.getAuthToken()
+                if (token.isNullOrEmpty()) {
+                    _error.value = "Не авторизован."
+                    return@launch
+                }
+                val response = repository.getUserSubscribers(userId, token)
+                _subscribeUsers.postValue(response)
+            } catch (e: NotAuthorizedException) {
+                _error.value = "Не авторизован."
+                preferencesManager.clearName()
+                preferencesManager.clearAuthToken()
+            } catch (e: BadRequestException) {
+                _error.value = "Некорректный запрос"
+            } catch (e: NetworkException) {
+                _error.value = "Проблемы с интернетом"
+            } catch (e: NotFoundException) {
+                _error.value = "Пользователь не найден"
+            } catch (e: Exception) {
+                _error.value = "Неизвестная ошибка"
+                Log.e("InfoComicViewModel", "Unknown error", e)
+            }
+        }
+    }
+
+    fun fetchSubscriptionsUsers(userId: String) {
+        _error.value = null
+        viewModelScope.launch {
+            try {
+                val token = preferencesManager.getAuthToken()
+                if (token.isNullOrEmpty()) {
+                    _error.value = "Не авторизован."
+                    return@launch
+                }
+                val response = repository.getUserSubscriptions(userId, token)
+                _subscribeUsers.postValue(response)
+            } catch (e: NotAuthorizedException) {
+                _error.value = "Не авторизован."
+                preferencesManager.clearName()
+                preferencesManager.clearAuthToken()
+            } catch (e: BadRequestException) {
+                _error.value = "Некорректный запрос"
             } catch (e: NetworkException) {
                 _error.value = "Проблемы с интернетом"
             } catch (e: NotFoundException) {
