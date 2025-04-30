c214aec3ffaaf47312e4e3dd304128d93d78a5b6
Serjik-Developer <urov51450@gmail.com>
2025-04-30 18:37:14 +0300

    small addition

--- app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/texnostrelka_2025_otbor/presentation/ui/settings/SettingsActivity.kt
@@ -2,9 +2,13 @@ package com.example.texnostrelka_2025_otbor.presentation.ui.settings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.texnostrelka_2025_otbor.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
